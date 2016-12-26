c8c3299d5fe1e8dff195d71be41a3ea9c26783f0
Hongye Liu <nicklhy@gmail.com>
2016-12-26 16:52:31 +0800

    fix a bug of saving network pic

--- visualize_network.py
+++ visualize_network.py
@@ -5,8 +5,8 @@ from model import get_memnn
 if not os.path.exists('cache'):
     os.mkdir('cache')
 
-sym = get_memnn(edim=150, mem_size=100, nwords=5000, nhop=6, lindim=75)
+sym = get_memnn(edim=150, mem_size=100, nwords=5000, nhop=1, lindim=150)
 a = mx.viz.plot_network(sym,
                         save_format='png',
                         shape={'data': (128, 150), 'time': (128, 100), 'target': (128,), 'context': (128, 100)})
-a.save('network_structure.png', 'cache')
+a.render('cache/network_structure')
