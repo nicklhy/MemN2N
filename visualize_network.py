6400103ba70fb03d1ba6a11ca53158868ab837cb
Hongye Liu <nicklhy@gmail.com>
2016-12-26 16:47:38 +0800

    fix the network pic's name

--- visualize_network.py
+++ visualize_network.py
@@ -9,4 +9,4 @@ sym = get_memnn(edim=150, mem_size=100, nwords=5000, nhop=6, lindim=75)
 a = mx.viz.plot_network(sym,
                         save_format='png',
                         shape={'data': (128, 150), 'time': (128, 100), 'target': (128,), 'context': (128, 100)})
-a.save('network.pdf', 'cache')
+a.save('network_structure.png', 'cache')
